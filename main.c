b5ee9a48e392b8f0ed1b4da0040fe2365df60083
Brulans4 <118511976+Brulans4@users.noreply.github.com>
2024-05-01 13:07:15 +0200

    feat: add main

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "comp.h"
+#include "cpu.h"
+#include "utils.h"
+
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        printf("missing argument <file to compile>\n");
+        exit(EXIT_FAILURE);
+    }
+    if (argc > 2)
+    {   
+        printf("too many arguments\n");
+        exit(EXIT_FAILURE);
+    }
+    compileProgram(argv[1]);
+    
+    CPU *cpu = cpuInit();
+
+    loadProgram(cpu);
+    executProgram(cpu);
+    printScreen(cpu);
+
+    free(cpu);
+}
